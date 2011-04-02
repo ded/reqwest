c6918d492a831022020dc37dddf8fc2404f7d076
Dustin Diaz <polvero@gmail.com>
2011-04-01 23:55:57 -0700

    add copyright and make functions normal looking

---
+++ src/copyright.js
@@ -0,0 +1,6 @@
+/*!
+  * Boom. Ajax! Ever heard of it!?
+  * copyright 2011 @dedfat
+  * https://github.com/ded/reqwest
+  * license MIT
+  */
--- src/reqwest.js
+++ src/reqwest.js
@@ -1,42 +1,33 @@
-/*!
-  * Boom. Ajax! Ever heard of it!?
-  * copyright 2011 @dedfat
-  * https://github.com/ded/reqwest
-  * license MIT
-  */
 !function (context) {
-  var twoHundo = /20\d/;
+  var twoHundo = /20\d/,
+      xhr = ('XMLHttpRequest' in window) ?
+        function() {
+          return new XMLHttpRequest;
+        } :
+        function() {
+          return new ActiveXObject('Microsoft.XMLHTTP');
+        };
 
-  var reqwest = function() {
-    function readyState (o, fn) {
-      return function () {
-        if (o && o.readyState == 4) {
-          if (twoHundo.test(o.status)) {
-            fn && typeof fn == 'function' ? fn(o) : fn.success(o);
-          } else {
-            fn && fn.error && fn.error(o);
-          }
-          fn && fn.complete && fn.complete(o);
+  function readyState (o, fn) {
+    return function () {
+      if (o && o.readyState == 4) {
+        if (twoHundo.test(o.status)) {
+          fn && typeof fn == 'function' ? fn(o) : fn.success(o);
+        } else {
+          fn && fn.error && fn.error(o);
         }
-      };
-    }
-
-    var xhr = ('XMLHttpRequest' in window) ?
-      function() {
-        return new XMLHttpRequest;
-      } :
-      function() {
-        return new ActiveXObject('Microsoft.XMLHTTP');
-      };
-
-    return function (o, callback) {
-      var http = xhr();
-      http.open(o.method || 'GET', typeof o == 'string' ? o : o.url, true);
-      http.onreadystatechange = readyState(http, callback || o);
-      http.send(o.data || null);
-      return http;
+        fn && fn.complete && fn.complete(o);
+      }
     };
-  }();
+  }
+
+  function reqwest (o, callback) {
+    var http = xhr();
+    http.open(o.method || 'GET', typeof o == 'string' ? o : o.url, true);
+    http.onreadystatechange = readyState(http, callback || o);
+    http.send(o.data || null);
+    return http;
+  }
 
   var oldJax = context.reqwest;
   reqwest.noConflict = function () {
