ab140dfa77e0b17a5f5f20f8ac147ad166838a5f
Dustin Diaz <polvero@gmail.com>
2011-05-26 14:55:39 -0700

    bug in serialize

--- src/ender.js
+++ src/ender.js
@@ -2,10 +2,10 @@ ender.ender({
   ajax: reqwest
 });
 ender.ender({
-  serialize: function (f) {
-    return reqwest.serialize(ender(f)[0]);
+  serialize: function () {
+    return reqwest.serialize(this[0]);
   }
-  , serializeArray: function(f) {
-    return reqwest.serializeArray(ender(f)[0]);
+  , serializeArray: function() {
+    return reqwest.serializeArray(this[0]);
   }
 }, true);
