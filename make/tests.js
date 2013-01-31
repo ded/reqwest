56a59de86460027b46e02cf1a49cfed3225418fd
Rod Vagg <rod@vagg.org>
2013-01-31 14:59:36 +1100

    .jshintrc, major reformat, fix bugs&tests in oldIE

--- make/tests.js
+++ make/tests.js
@@ -1,10 +1,10 @@
-var http = require('http')
-  , exec = require('child_process').exec
+var exec = require('child_process').exec
   , fs = require('fs')
   , Connect = require('connect')
   , dispatch = require('dispatch')
   , mime = require('mime')
   , DelayedStream = require('delayed-stream')
+
   , getMime = function(ext) {
       return mime.lookup(ext == 'jsonp' ? 'js' : ext)
     }
@@ -15,17 +15,17 @@ var routes = {
     res.end()
   },
   '/tests/timeout$': function (req, res) {
-      var delayed = DelayedStream.create(req);
-	  setTimeout(function() {
-	      res.writeHead(200, {
-	          'Expires': 0
-	        , 'Cache-Control': 'max-age=0, no-cache, no-store'
-	      })
-	      req.query.callback && res.write(req.query.callback + '(')
-	      res.write(JSON.stringify({ method: req.method, query: req.query, headers: req.headers }))
-          req.query.callback && res.write(');')
-	      delayed.pipe(res)
-	  }, 2000)
+      var delayed = DelayedStream.create(req)
+      setTimeout(function() {
+        res.writeHead(200, {
+            'Expires': 0
+          , 'Cache-Control': 'max-age=0, no-cache, no-store'
+        })
+        req.query.callback && res.write(req.query.callback + '(')
+        res.write(JSON.stringify({ method: req.method, query: req.query, headers: req.headers }))
+        req.query.callback && res.write(');')
+        delayed.pipe(res)
+      }, 2000)
   },
   '(([\\w\\-\\/]+)\\.(css|js|json|jsonp|html|xml)$)': function (req, res, next, uri, file, ext) {
     res.writeHead(200, {
@@ -49,14 +49,14 @@ Connect.createServer(Connect.query(), dispatch(routes)).listen(1234)
 var otherOriginRoutes = {
     '/get-value': function (req, res) {
       res.writeHead(200, {
-        'Access-Control-Allow-Origin': 'http://localhost:1234',
+        'Access-Control-Allow-Origin': req.headers.origin,
         'Content-Type': 'text/plain'
       })
       res.end('hello')
     },
     '/set-cookie': function (req, res) {
       res.writeHead(200, {
-        'Access-Control-Allow-Origin': 'http://localhost:1234',
+        'Access-Control-Allow-Origin': req.headers.origin,
         'Access-Control-Allow-Credentials': 'true',
         'Content-Type': 'text/plain',
         'Set-Cookie': 'cookie=hello'
@@ -64,15 +64,17 @@ var otherOriginRoutes = {
       res.end('Set a cookie!')
     },
     '/get-cookie-value': function (req, res) {
-      var cookies = {};
+      var cookies = {}
+        , value
+
       req.headers.cookie && req.headers.cookie.split(';').forEach(function( cookie ) {
-        var parts = cookie.split('=');
-        cookies[ parts[ 0 ].trim() ] = ( parts[ 1 ] || '' ).trim();
-      });
-      var value = cookies.cookie
+        var parts = cookie.split('=')
+        cookies[ parts[ 0 ].trim() ] = ( parts[ 1 ] || '' ).trim()
+      })
+      value = cookies.cookie
       
       res.writeHead(200, {
-          'Access-Control-Allow-Origin': 'http://localhost:1234',
+          'Access-Control-Allow-Origin': req.headers.origin,
           'Access-Control-Allow-Credentials': 'true',
           'Content-Type': 'text/plain'
       })
--- src/reqwest.js
+++ src/reqwest.js
@@ -1,4 +1,4 @@
-(function (name, context, definition) {
+;(function (name, context, definition) {
   if (typeof module != 'undefined' && module.exports) module.exports = definition()
   else if (typeof define == 'function' && define.amd) define(definition)
   else context[name] = definition()
@@ -18,68 +18,78 @@
     , xmlHttpRequest = 'XMLHttpRequest'
     , noop = function () {}
 
-  var isArray = typeof Array.isArray == 'function' ? Array.isArray : function (a) {
-    return a instanceof Array
-  }
-  var defaultHeaders = {
-      contentType: 'application/x-www-form-urlencoded'
-    , requestedWith: xmlHttpRequest
-    , accept: {
-        '*':  'text/javascript, text/html, application/xml, text/xml, */*'
-      , xml:  'application/xml, text/xml'
-      , html: 'text/html'
-      , text: 'text/plain'
-      , json: 'application/json, text/javascript'
-      , js:   'application/javascript, text/javascript'
-      }
-    }
-  var xhr = win[xmlHttpRequest] ?
-    function () {
-      return new XMLHttpRequest()
-    } :
-    function () {
-      return new ActiveXObject('Microsoft.XMLHTTP')
-    }
+    , isArray = typeof Array.isArray == 'function'
+        ? Array.isArray
+        : function (a) {
+            return a instanceof Array
+          }
 
-  function handleReadyState(o, success, error) {
+    , defaultHeaders = {
+          contentType: 'application/x-www-form-urlencoded'
+        , requestedWith: xmlHttpRequest
+        , accept: {
+              '*':  'text/javascript, text/html, application/xml, text/xml, */*'
+            , xml:  'application/xml, text/xml'
+            , html: 'text/html'
+            , text: 'text/plain'
+            , json: 'application/json, text/javascript'
+            , js:   'application/javascript, text/javascript'
+          }
+      }
+
+    , xhr = win[xmlHttpRequest]
+        ? function () {
+            return new XMLHttpRequest()
+          }
+        : function () {
+            return new ActiveXObject('Microsoft.XMLHTTP')
+          }
+
+  function handleReadyState (r, success, error) {
     return function () {
-      if (o && o[readyState] == 4) {
-        o.onreadystatechange = noop;
-        if (twoHundo.test(o.status)) {
-          success(o)
-        } else {
-          error(o)
-        }
+      // use _aborted to mitigate against IE err c00c023f
+      // (can't read props on aborted request objects)
+      if (r._aborted) return error(r.request)
+      if (r.request && r.request[readyState] == 4) {
+        r.request.onreadystatechange = noop
+        if (twoHundo.test(r.request.status))
+          success(r.request)
+        else
+          error(r.request)
       }
     }
   }
 
-  function setHeaders(http, o) {
-    var headers = o.headers || {}, h
-    headers.Accept = headers.Accept || defaultHeaders.accept[o.type] || defaultHeaders.accept['*']
+  function setHeaders (http, o) {
+    var headers = o.headers || {}
+      , h
+
+    headers.Accept = headers.Accept
+      || defaultHeaders.accept[o.type]
+      || defaultHeaders.accept['*']
+
     // breaks cross-origin requests with legacy browsers
     if (!o.crossOrigin && !headers[requestedWith]) headers[requestedWith] = defaultHeaders.requestedWith
     if (!headers[contentType]) headers[contentType] = o.contentType || defaultHeaders.contentType
-    for (h in headers) {
+    for (h in headers)
       headers.hasOwnProperty(h) && http.setRequestHeader(h, headers[h])
-    }
   }
 
-  function setCredentials(http, o) {
-    if (typeof o.withCredentials !== "undefined" && typeof http.withCredentials !== "undefined") {
+  function setCredentials (http, o) {
+    if (typeof o.withCredentials !== 'undefined' && typeof http.withCredentials !== 'undefined') {
       http.withCredentials = !!o.withCredentials
     }
   }
 
-  function generalCallback(data) {
+  function generalCallback (data) {
     lastValue = data
   }
 
-  function urlappend(url, s) {
+  function urlappend (url, s) {
     return url + (/\?/.test(url) ? '&' : '?') + s
   }
 
-  function handleJsonp(o, fn, err, url) {
+  function handleJsonp (o, fn, err, url) {
     var reqId = uniqid++
       , cbkey = o.jsonpCallback || 'callback' // the 'callback' key
       , cbval = o.jsonpCallbackName || reqwest.getcallbackPrefix(reqId)
@@ -130,9 +140,8 @@
 
     // Add the script to the DOM head
     head.appendChild(script)
-    
-    
-    // Enable JSONP timeout	
+
+    // Enable JSONP timeout
     return {
       abort: function () {
         script.onload = script.onreadystatechange = null
@@ -144,8 +153,9 @@
     }
   }
 
-  function getRequest(o, fn, err) {
-    var method = (o.method || 'GET').toUpperCase()
+  function getRequest (fn, err) {
+    var o = this.o
+      , method = (o.method || 'GET').toUpperCase()
       , url = typeof o === 'string' ? o : o.url
       // convert non-string objects to query-string form unless o.processData is false
       , data = (o.processData !== false && o.data && typeof o.data !== 'string')
@@ -166,25 +176,25 @@
     http.open(method, url, true)
     setHeaders(http, o)
     setCredentials(http, o)
-    http.onreadystatechange = handleReadyState(http, fn, err)
+    http.onreadystatechange = handleReadyState(this, fn, err)
     o.before && o.before(http)
     http.send(data)
     return http
   }
 
-  function Reqwest(o, fn) {
+  function Reqwest (o, fn) {
     this.o = o
     this.fn = fn
 
     init.apply(this, arguments)
   }
 
-  function setType(url) {
+  function setType (url) {
     var m = url.match(/\.(json|jsonp|html|xml)(\?|$)/)
     return m ? m[1] : 'js'
   }
 
-  function init(o, fn) {
+  function init (o, fn) {
 
     this.url = typeof o == 'string' ? o : o.url
     this.timeout = null
@@ -230,7 +240,7 @@
       })
     }
 
-    function complete(resp) {
+    function complete (resp) {
       o.timeout && clearTimeout(self.timeout)
       self.timeout = null
       while (self._completeHandlers.length > 0) {
@@ -238,7 +248,7 @@
       }
     }
 
-    function success(resp) {
+    function success (resp) {
       var r = resp.responseText
       if (r) {
         switch (type) {
@@ -248,16 +258,21 @@
           } catch (err) {
             return error(resp, 'Could not parse JSON in response', err)
           }
-          break;
+          break
         case 'js':
           resp = eval(r)
-          break;
+          break
         case 'html':
           resp = r
-          break;
+          break
         case 'xml':
-          resp = resp.responseXML;
-          break;
+          resp = resp.responseXML
+              && resp.responseXML.parseError // IE trololo
+              && resp.responseXML.parseError.errorCode
+              && resp.responseXML.parseError.reason
+            ? null
+            : resp.responseXML
+          break
         }
       }
 
@@ -271,7 +286,7 @@
       complete(resp)
     }
 
-    function error(resp, msg, t) {
+    function error (resp, msg, t) {
       self._responseArgs.resp = resp
       self._responseArgs.msg = msg
       self._responseArgs.t = t
@@ -282,11 +297,12 @@
       complete(resp)
     }
 
-    this.request = getRequest(o, success, error)
+    this.request = getRequest.call(this, success, error)
   }
 
   Reqwest.prototype = {
     abort: function () {
+      this._aborted = true
       this.request.abort()
     }
 
@@ -339,16 +355,16 @@
     }
   }
 
-  function reqwest(o, fn) {
+  function reqwest (o, fn) {
     return new Reqwest(o, fn)
   }
 
   // normalize newline variants according to spec -> CRLF
-  function normalize(s) {
+  function normalize (s) {
     return s ? s.replace(/\r?\n/g, '\r\n') : ''
   }
 
-  function serial(el, cb) {
+  function serial (el, cb) {
     var n = el.name
       , t = el.tagName.toLowerCase()
       , optCb = function (o) {
@@ -357,47 +373,49 @@
           if (o && !o.disabled)
             cb(n, normalize(o.attributes.value && o.attributes.value.specified ? o.value : o.text))
         }
+      , ch, ra, val, i
 
     // don't serialize elements that are disabled or without a name
-    if (el.disabled || !n) return;
+    if (el.disabled || !n) return
 
     switch (t) {
     case 'input':
       if (!/reset|button|image|file/i.test(el.type)) {
-        var ch = /checkbox/i.test(el.type)
-          , ra = /radio/i.test(el.type)
-          , val = el.value;
+        ch = /checkbox/i.test(el.type)
+        ra = /radio/i.test(el.type)
+        val = el.value
         // WebKit gives us "" instead of "on" if a checkbox has no value, so correct it here
-        (!(ch || ra) || el.checked) && cb(n, normalize(ch && val === '' ? 'on' : val))
+        ;(!(ch || ra) || el.checked) && cb(n, normalize(ch && val === '' ? 'on' : val))
       }
-      break;
+      break
     case 'textarea':
       cb(n, normalize(el.value))
-      break;
+      break
     case 'select':
       if (el.type.toLowerCase() === 'select-one') {
         optCb(el.selectedIndex >= 0 ? el.options[el.selectedIndex] : null)
       } else {
-        for (var i = 0; el.length && i < el.length; i++) {
+        for (i = 0; el.length && i < el.length; i++) {
           el.options[i].selected && optCb(el.options[i])
         }
       }
-      break;
+      break
     }
   }
 
   // collect up all form elements found from the passed argument elements all
   // the way down to child elements; pass a '<form>' or form fields.
   // called with 'this'=callback to use for serial() on each element
-  function eachFormElement() {
+  function eachFormElement () {
     var cb = this
-      , e, i, j
+      , e, i
       , serializeSubtags = function (e, tags) {
-        for (var i = 0; i < tags.length; i++) {
-          var fa = e[byTag](tags[i])
-          for (j = 0; j < fa.length; j++) serial(fa[j], cb)
+          var i, j, fa
+          for (i = 0; i < tags.length; i++) {
+            fa = e[byTag](tags[i])
+            for (j = 0; j < fa.length; j++) serial(fa[j], cb)
+          }
         }
-      }
 
     for (i = 0; i < arguments.length; i++) {
       e = arguments[i]
@@ -407,12 +425,12 @@
   }
 
   // standard query string style serialization
-  function serializeQueryString() {
+  function serializeQueryString () {
     return reqwest.toQueryString(reqwest.serializeArray.apply(null, arguments))
   }
 
   // { 'name': 'value', ... } style serialization
-  function serializeHash() {
+  function serializeHash () {
     var hash = {}
     eachFormElement.apply(function (name, value) {
       if (name in hash) {
@@ -454,13 +472,14 @@
       , push = function (k, v) {
           qs += enc(k) + '=' + enc(v) + '&'
         }
+      , k, v
 
     if (isArray(o)) {
       for (i = 0; o && i < o.length; i++) push(o[i].name, o[i].value)
     } else {
-      for (var k in o) {
-        if (!Object.hasOwnProperty.call(o, k)) continue;
-        var v = o[k]
+      for (k in o) {
+        if (!Object.hasOwnProperty.call(o, k)) continue
+        v = o[k]
         if (isArray(v)) {
           for (i = 0; i < v.length; i++) push(k, v[i])
         } else push(k, o[k])
@@ -471,7 +490,7 @@
     return qs.replace(/&$/, '').replace(/%20/g, '+')
   }
 
-  reqwest.getcallbackPrefix = function (reqId) {
+  reqwest.getcallbackPrefix = function () {
     return callbackPrefix
   }
 
@@ -488,4 +507,4 @@
   }
 
   return reqwest
-});
+})
